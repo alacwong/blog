a8c028a574b1aa1226f78978e5de7ef9afa1c0f4
alacwong <46137625+alacwong@users.noreply.github.com>
2020-05-31 14:19:18 -0400

    removed unecessary import

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Login from "./components/login"
-import CreateAccount from "./components/CreateAccount"
 import "bootstrap/dist/css/bootstrap.min.css"
 // import { BrowserRouter  as Router, Route} from 'react-router-dom'
 function App() {
